9a2fe6adb42be2f22f19fc85365cf930394d3504
lijuheng007 <1003719724@qq.com>
2020-07-21 00:26:34 +0800

    第二次提交

--- src/main/java/com/java/GittestApplication.java
+++ src/main/java/com/java/GittestApplication.java
@@ -9,6 +9,7 @@ public class GittestApplication {
     public static void main(String[] args) {
         SpringApplication.run(GittestApplication.class, args);
         System.out.println("呵呵");
+        System.out.println("呵呵2222");
     }
 
 }
